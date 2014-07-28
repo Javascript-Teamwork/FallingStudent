5a8135c2325613ffc0afda93c3d08e26d7d4b5d1
Yavor Mitev <yavor2000@yahoo.com>
2014-07-28 22:48:54 +0300

    added shuffle function to shuffle the deck of cards

--- Solitaire/scripts/games-solitaire.js
+++ Solitaire/scripts/games-solitaire.js
@@ -8,5 +8,20 @@ function log(msg) {
 (function () {
 
     var deck = [52];
+    var suits = [ '\u2663', '\u2666', '\u2665', '\u2660' ];
+    var cards = "2 3 4 5 6 7 8 9 10 J D K A".split(" ");
+    for (var i = 0, cnt = 0; i < cards.length; i++) {
+        for (var j = 0; j < suits.length; j++, cnt++) {
+            deck[cnt] = cards[i] + suits[j];
+        }
+    }
+    //lets shuffle the deck
+    shuffle(deck);
+    console.log(deck);
 
 }());
+
+function shuffle(array){
+    for(var j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x) {}
+    return array;
+}
