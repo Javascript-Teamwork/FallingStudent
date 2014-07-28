22dd41dcdb335e3c21d84cfb920284a423e8ccd0
Vassil Nikov <vassiln@yahoo.com>
2014-07-28 23:07:38 +0300

    created the decks to hold the cards

--- Solitaire/scripts/games-solitaire.js
+++ Solitaire/scripts/games-solitaire.js
@@ -7,9 +7,21 @@ function log(msg) {
 */
 (function () {
 
-    var deck = [52];
+    var deck = new Array(52);
+	var firstColumn = new Array(1);
+	var secondColumn = new Array(2);
+	var thirdColumn = new Array(3);
+	var fourthColumn = new Array(4);
+	var fifthColumn = new Array(5);
+	var sixthColumn = new Array(6);
+	var seventhColumn = new Array(7);
+	var completedSuitOne = new Array(13);
+	var completedSuitTwo = new Array(13);
+	var completedSuitThree = new Array(13);
+	var completedSuitFour = new Array(13);
     var suits = [ '\u2663', '\u2666', '\u2665', '\u2660' ];
     var cards = "2 3 4 5 6 7 8 9 10 J D K A".split(" ");
+	
     for (var i = 0, cnt = 0; i < cards.length; i++) {
         for (var j = 0; j < suits.length; j++, cnt++) {
             deck[cnt] = cards[i] + suits[j];
