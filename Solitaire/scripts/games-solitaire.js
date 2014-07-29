bcebdbaa4821a7042d9e1ffd1b54bd97aff6a434
Vassil Nikov <vassiln@yahoo.com>
2014-07-30 00:07:21 +0300

    fixing a bug

--- Solitaire/scripts/games-solitaire.js
+++ Solitaire/scripts/games-solitaire.js
@@ -8,31 +8,30 @@ function log(msg) {
 (function () {
 
     var deck = new Array(52);
-	var firstColumn = new Array(1);
-	var secondColumn = new Array(2);
-	var thirdColumn = new Array(3);
-	var fourthColumn = new Array(4);
-	var fifthColumn = new Array(5);
-	var sixthColumn = new Array(6);
-	var seventhColumn = new Array(7);
-	var completedSuitOne = new Array(13);
-	var completedSuitTwo = new Array(13);
-	var completedSuitThree = new Array(13);
-	var completedSuitFour = new Array(13);
+    var firstColumn = new Array(1);
+    var secondColumn = new Array(2);
+    var thirdColumn = new Array(3);
+    var fourthColumn = new Array(4);
+    var fifthColumn = new Array(5);
+    var sixthColumn = new Array(6);
+    var seventhColumn = new Array(7);
+    var completedSuitOne = new Array(13);
+    var completedSuitTwo = new Array(13);
+    var completedSuitThree = new Array(13);
+    var completedSuitFour = new Array(13);
     var suits = [ '\u2663', '\u2666', '\u2665', '\u2660' ];
     var cards = "2 3 4 5 6 7 8 9 10 J D K A".split(" ");
 	var lengthCards = cards.length;
-	var lengthSuits = suits.length;
-	
+    var lengthSuits = suits.length;
+
     for (var i = 0, cnt = 0; i < lengthCards; i++) {
         for (var j = 0; j < lengthSuits; j++, cnt++) {
             deck[cnt] = cards[i] + suits[j];
         }
     }
-	
     //lets shuffle the deck
     shuffle(deck);
-	 //Let's spread the wealth(i.e. the cards) :)
+    //Let's spread the wealth(i.e. the cards) :)
     fillInArrays(firstColumn, deck);
     fillInArrays(secondColumn, deck);
     fillInArrays(thirdColumn, deck);
@@ -49,11 +48,11 @@ function shuffle(array){
     return array;
 }
 
-function fillInArrays (deck, arr) {
-	var n = arr.length;
-	
-	for (var i = 0; i < n; i++) {
-		var card = deck.pop();
-		arr[i] = card;
-	}
+function fillInArrays (arr, deck) {
+    var n = arr.length;
+
+    for (var i = 0; i < n; i++) {
+        var card = deck.pop();
+        arr[i] = card;
+    }
 }
